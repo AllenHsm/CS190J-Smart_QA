c4d107d7c2b2fe045e33ae731eac5330441ebadd
RayDu <97069739+CongruiDu@users.noreply.github.com>
2024-06-02 17:13:00 -0700

    Co-authored-by: ZixiaoJin <ZixiaoJin@users.noreply.github.com>

--- Contracts.sol
+++ Contracts.sol
@@ -1,167 +1,226 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-contract SurveyList {
-    
-    address owner;
-    address[] participant_list;
-    string[] accountNames; 
-    
-    // These are for our models' ids.
+contract SmartQA {
+    address public owner;
+    User[] public users;
+    string[] public accountNames;
+
+    // State mappings
+    mapping(address => User) public userAddrMap;
+    mapping(address => bool) public hasRegistered;
+    mapping(uint256 => Question) public questionMap;
+    mapping(uint256 => Answer) public answerMap;
+
+    // Models' ids.
     uint256 public questionCount = 0;
-    uint256 public ParticipantCount = 0;
-    uint256 public AnswerCount = 0;
-    uint256 public EndorsementCount = 0;
+    uint256 public userCount = 0;
+    uint256 public answerCount = 0;
+    // Event declaration
+    event UserRegistered(string username, address userAddress);
 
-    // ---------- These are our models. Such as Question, Participant, Answer ----------
-    
-    struct Question{
-        uint256 id;
+    // Structs for data models
+    struct Question {
+        address asker;
         string content;
-        string[] options;
-        bool q_isfull;
-        uint256 start_time;
-        uint256 end_time;
+        uint256 reward;
+        uint256 expiration_time;
+        uint256 question_id;
+        bool closed;
+        uint256[] answer_ids;
     }
-    
-    struct Participant{
-        address p_address;
-        string name;
-        uint256 age;
-        bool isfull;
+
+    struct User {
+        string user_name;
+        address user_address;
     }
-    
-    struct Answer{
+
+    struct Answer {
+        address answerer;
         uint256 answer_id;
-        address who_participated;
-        string[] a_answer;
-        bool isSelected; 
+        uint256 question_id;
+        string content;
+        bool isSelected;
+        address[] endorsers;
     }
 
-    struct Endorsement{
-        uint256 endorsement_id;
-        address who_endorsed;
-        uint256 question_id; 
-    }
-    
-    
-    // ---------- Events for creating question, and participant ----------
-    event QuestionCreated(uint256 id, string _content, string[] answer);
-    event ParticipantCreated(address p_address, string name, uint256 age , bool isfull);
-    event AnswerCreated(uint256 answer_id, address who_participated, string[] a_answer, bool isSelected);
-    event EndorsementCreated(uint256 endorsement_id, address who_endorsed, uint256 question_id);
-
-    
-    // ---------- Mappings to save relevant data for Question, Participant, Answer, Endorsement ----------
-    mapping(uint256 => Question) public questions;
-    mapping(address => Participant) public participants;
-    mapping(uint256 => Answer) public answers;
-    mapping (uint256 => Endorsement) public endorsements; 
-    
-    // ---------- Mappings to save questions for survey, surveylist for participant, endorsements for question ----------
-    mapping (uint256 => uint256[]) public endorsements_of_question;
-    
-    
-    constructor(){
+    constructor() {
         owner = msg.sender;
     }
-    
-    modifier ownerOnly {
-        require(msg.sender == owner, "You are not an owner.");
-        _;
-    }
+    Question[] public questions;
 
-    modifier createdQuestionBefore(string memory _content, string[] memory answer){
+    //  -------------------------------------------- modifiers  --------------------------------------------
+    modifier isRegistered(address addr) {
         bool flag = true;
-        for(uint256 i = 0; i <= questionCount; i++){
-            if(keccak256(abi.encodePacked(questions[i].content)) == keccak256(abi.encodePacked(_content)) && answer.length ==  questions[i].options.length){
+        for (uint i = 0; i < userCount; i++) {
+            if (users[i].user_address == addr) {
                 flag = false;
             }
         }
-        require(flag, "This question created before!");
+        require(flag, "The address was registered");
         _;
-    }   
-    
+    }
 
-    modifier endorsedBefore(address _address, uint256 QuestionId, uint256 AnswerId){
+    modifier isNameDuplicate(string memory name) {
         bool flag = true;
-        for (uint256 i = 0; i < endorsements_of_question[QuestionId].length; i++){
-            if(endorsements_of_question[QuestionId][i] == AnswerId){
+        for (uint i = 0; i < userCount; i++) {
+            if (
+                keccak256(bytes(users[i].user_name)) == keccak256(bytes(name))
+            ) {
                 flag = false;
             }
         }
-        require(flag, "You have endorsed this answer before!"); 
-        _; 
+        require(flag, "The username is duplicate, please try another one");
+        _;
     }
-    
 
-    
-    // ---------- Creating functions ----------
-    function createQuestion(string memory _content, string[] memory answer,uint256 start, uint256 end) public ownerOnly createdQuestionBefore(_content,answer){
-        require(bytes(_content).length > 0 && answer.length>0, "Please Fill In The Blanks");
-        questions[questionCount] = Question(questionCount, _content, answer ,true, start, end);
-        emit QuestionCreated(questionCount, _content, answer);
+    modifier isEndorsedBySameUser(address addr, uint256 answer_id) {
+        bool flag = true;
+        for (uint i = 0; i < answerMap[answer_id].endorsers.length; i++) {
+            if (answerMap[answer_id].endorsers[i] == addr) {
+                flag = false;
+            }
+        }
+        require(flag, "You can only endorse the same question once");
+        _;
+    }
+
+    //  -------------------------------------------- Accesser  --------------------------------------------
+    function getQuestion(uint256 q_id) public view returns (string memory) {
+        Question memory question = questionMap[q_id];
+        return (question.content);
+    }
+    function getAnswers(uint256 a_id) public view returns (string memory) {
+        Answer memory answer = answerMap[a_id];
+        return (answer.content);
+    }
+    function getNumOfEndorse(uint256 a_id) public view returns (uint256) {
+        Answer memory answer = answerMap[a_id];
+        return (answer.endorsers.length);
+    }
+    function isAnswerSelected(uint a_id) public view returns (bool) {
+        Answer memory answer = answerMap[a_id];
+        return answer.isSelected;
+    }
+    function getDuration(uint256 q_id) public view returns (uint256) {
+        Question memory question = questionMap[q_id];
+        return (question.expiration_time);
+    }
+    function getParticipantCount(uint q_id) public view returns (uint256) {
+        Question memory question = questionMap[q_id];
+        return question.answer_ids.length;
+    }
+    function getParticipantAddr () public view returns (address){
+        return msg.sender; 
+    }
+    function getNumOfEndorsement(uint256 a_id) public view returns (uint256) {
+        Answer memory answer = answerMap[a_id];
+        return answer.endorsers.length;
+    }
+    // ------------------------------------------- Update functions ----------------------------------------------
+    function selectAnswer(uint256 a_id) public {
+        require(
+            hasRegistered[msg.sender],
+            "User must be registered to select an answer"
+        );
+        answerMap[a_id].isSelected = true;
+    }
+    function closeQuestion(uint256 q_id) public {
+        require(
+            hasRegistered[msg.sender],
+            "User must be registered to close a question"
+        );
+        questionMap[q_id].closed = true;
+    }
+    // ------------------------------------------- Create functions ----------------------------------------------
+    function registerUser(string memory _username) public {
+        require(bytes(_username).length > 0, "Username cannot be empty");
+        require(!hasRegistered[msg.sender], "Address already registered");
+
+        User memory newUser = User(_username, msg.sender);
+        users.push(newUser);
+        userAddrMap[msg.sender] = newUser;
+        accountNames.push(_username);
+        hasRegistered[msg.sender] = true;
+        userCount++;
+
+        emit UserRegistered(_username, msg.sender);
+    }
+
+    function askQuestion(
+        string memory _content,
+        uint256 _reward,
+        uint256 _expirationTime
+    ) public {
+        require(
+            hasRegistered[msg.sender],
+            "User must be registered to ask a question"
+        );
+        require(_reward > 0, "Reward must be greater than 0");
+        require(bytes(_content).length > 0, "Question content cannot be empty");
+
+        Question memory newQuestion = Question(
+            msg.sender,
+            _content,
+            _reward,
+            block.timestamp + _expirationTime,
+            questionCount,
+            false,
+            new uint256[](0)
+        );
+        questions.push(newQuestion);
         questionCount++;
     }
-    
-    function createParticipant(string memory name, uint256 age) public{
-        require(bytes(name).length > 0 && age > 0, "Please Fill In The Blanks");
-        require(participants[msg.sender].isfull == false, "You don't have participant account / You can't create duplicate account.");
-        participants[msg.sender] = Participant(msg.sender, name, age, true);
-        emit ParticipantCreated(msg.sender, name, age ,true);
-        participant_list.push(msg.sender);
+
+    function postAnswer(
+        uint256 answer_id,
+        uint256 question_id,
+        string memory content
+    ) public {
+        require(
+            hasRegistered[msg.sender],
+            "User must be registered to answer a question"
+        );
+        require(bytes(content).length > 0, "Answer content cannot be empty");
+        address[] memory endorsers;
+        Answer memory newAnswer = Answer(
+            msg.sender,
+            answer_id,
+            question_id,
+            content,
+            false,
+            endorsers
+        );
+        answerMap[answer_id] = newAnswer;
+        questionMap[question_id].answer_ids.push(answer_id);
+        answerCount++;
     }
-    
-    function EndorseAnswer(uint256 answer, uint256 QuestionId) public{
-        require(answers[answer].who_participated == msg.sender, "You can't endorse this answer!");
-        require(answers[answer].isSelected == false, "You have already endorsed this answer!");
-        endorsements[EndorsementCount] = Endorsement(EndorsementCount, msg.sender, QuestionId);
-        endorsements_of_question[QuestionId].push(answer);
-        answers[answer].isSelected = true; 
-        EndorsementCount++;
-        emit EndorsementCreated(EndorsementCount, msg.sender, QuestionId);
-    }
-    function AdwardEther(address _address, uint256 amount) public ownerOnly {
-        payable(_address).transfer(amount);
-    }
-    
-    // ---------- Getter functions ----------
-    
-    
-    
-    function getQuestion(uint256 id) public view returns (string memory, string[] memory q_answers){
-        Question memory question = questions[id];
-        return (question.content,question.options);
-    }
-    
-    function getParticipantCount() public view returns (uint256){
-        return participant_list.length;
-    } 
-    
-    function getParticipantAddress() public view returns (address){
-        return msg.sender;
-    } 
-    
-    
-    function getAnswer(uint256 a_id) public view returns (string[] memory ans){
-        ans = answers[a_id].a_answer;
-        return ans;
-    }
-    
-    function getQuestionCount() public view returns (uint256){
-        return questionCount;
-    }
-    function getUserWithMostEndorsement(uint256 questionId) public view returns (address){
-        uint256 max = 0;
-        address user;
-        for (uint256 i = 0; i < endorsements_of_question[questionId].length; i++){
-            uint256 answerId = endorsements_of_question[questionId][i];
-            if(answers[answerId].a_answer.length > max){
-                max = answers[answerId].a_answer.length;
-                user = answers[answerId].who_participated;
+
+    function endorse(
+        uint256 answer_id,
+        uint256 question_id
+    ) public {
+        require(
+            hasRegistered[msg.sender],
+            "User must be registered to add endorsements"
+        );
+        require(
+            questionMap[question_id].answer_ids.length > 0,
+            "There is no answer to endorse"
+        );
+        address[] memory ansEndorsers = answerMap[answer_id].endorsers;
+        bool hasEndorsed = false;
+        for (uint i = 0; i < ansEndorsers.length; i++) {
+            if (ansEndorsers[i] == msg.sender) {
+                hasEndorsed = true;
             }
         }
-        return user;
+        require(
+            !hasEndorsed,
+            "User can not endorse an answer twice"
+        );
+
+        answerMap[answer_id].endorsers.push(msg.sender);
     }
 
     
